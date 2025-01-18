6baad1f32e26d1486b1e2b7dd60260f40696030b
Muhammed Yusuf <77261036+MyyTimes@users.noreply.github.com>
2025-01-18 23:50:12 +0300

    Add files via upload

--- BoardLayoutAndPieces.h
+++ BoardLayoutAndPieces.h
@@ -3,13 +3,11 @@
 
 struct chessPiece
 {
-    //int isWhite; //Is the piece white or black?
     int startingPosition[2];
     int instantPosition[2];
     char symbol;
     int firstMove;
-
-    //char pieceName[15];
+    int isWhite;
 };
 
 void CreatePawns(struct chessPiece *piece, int yxPosition)
@@ -20,70 +18,99 @@ void CreatePawns(struct chessPiece *piece, int yxPosition)
     piece->instantPosition[0] = piece->startingPosition[0];
     piece->instantPosition[1] = piece->startingPosition[1];
 }
-
+/*
 void ChangePositionOfPiece(struct chessPiece *piece, int newPosition)
 {
     piece->instantPosition[0] = newPosition / 10;
     piece->instantPosition[1] = newPosition % 10;
 }
-
+*/
 void LocatePieceOnBoard(struct chessPiece *piece, char board[8][8])
 {
     board[piece->instantPosition[0]][piece->instantPosition[1]] = piece->symbol;
 }
 
-void MovePieceAndSetBoard(struct chessPiece *piece, char chessBoard[8][8], int nextPosition, char defaultSymbol)
+void MovePieceAndSetBoard(struct chessPiece **piece, char chessBoard[8][8], int nextPosition, char defaultSymbol)
 {
-    chessBoard[piece->instantPosition[0]][piece->instantPosition[1]] = defaultSymbol; //Clear old position
-    ChangePositionOfPiece(piece, nextPosition); //Save new position in struct
-    chessBoard[piece->instantPosition[0]][piece->instantPosition[1]] = piece->symbol;
+    chessBoard[(*piece)->instantPosition[0]][(*piece)->instantPosition[1]] = defaultSymbol; //Clear old position
+    //ChangePositionOfPiece(piece, nextPosition); 
+    //Save new position in struct
+    (*piece)->instantPosition[0] = nextPosition / 10;
+    (*piece)->instantPosition[1] = nextPosition % 10;
+    chessBoard[(*piece)->instantPosition[0]][(*piece)->instantPosition[1]] = (*piece)->symbol;
 }
 
-int PawnMotion(struct chessPiece *pawn, int nextPosition)
+int PawnMotion(struct chessPiece **pawn, int nextPosition)
 {
-    if((pawn->instantPosition[0] - nextPosition / 10 == 1)&& (pawn->instantPosition[1] == nextPosition % 10))
+    if((*pawn)->isWhite == 1)
     {
-        if(pawn->firstMove == 1)
+        if( ((*pawn)->instantPosition[0] - nextPosition / 10 == 1) && ((*pawn)->instantPosition[1] == nextPosition % 10))
         {
-            pawn->firstMove = 0;
-        }
+            if((*pawn)->firstMove == 1)
+            {
+                (*pawn)->firstMove = 0;
+            }
 
-        return 1;
+            return 1;
+        }
+        
+        if(((*pawn)->instantPosition[0] - nextPosition / 10 == 2) && ((*pawn)->instantPosition[1] == nextPosition % 10) && (*pawn)->firstMove)
+        {
+            if((*pawn)->firstMove == 1)
+            {
+                (*pawn)->firstMove = 0;
+            }
+
+            return 1;
+        }
     }
-    else if((pawn->instantPosition[0] - nextPosition / 10 == 2) && (pawn->instantPosition[1] == nextPosition % 10) && pawn->firstMove)
+    if((*pawn)->isWhite == 0)
     {
-        if(pawn->firstMove == 1)
+        if( ((*pawn)->instantPosition[0] - nextPosition / 10 == -1) && ((*pawn)->instantPosition[1] == nextPosition % 10))
         {
-            pawn->firstMove = 0;
+            if((*pawn)->firstMove == 1)
+            {
+                (*pawn)->firstMove = 0;
+            }
+
+            return 1;
         }
+        
+        if(((*pawn)->instantPosition[0] - nextPosition / 10 == -2) && ((*pawn)->instantPosition[1] == nextPosition % 10) && (*pawn)->firstMove)
+        {
+            if((*pawn)->firstMove == 1)
+            {
+                (*pawn)->firstMove = 0;
+            }
 
-        return 1;
+            return 1;
+        }
     }
-
+    
     return 0;
 }
 
-int RookMotion(struct chessPiece *rook, char chessBoard[8][8], int nextPos, char defaultSymbol)
+int RookMotion(struct chessPiece **rook, char chessBoard[8][8], int nextPos, char defaultSymbol)
 {   
     int nextPosition[] = {nextPos / 10, nextPos % 10};
 
-    if(nextPosition[0] == rook->instantPosition[0])
+    if(nextPosition[0] == (*rook)->instantPosition[0])
     {
-        int step = (nextPosition[1] - rook->instantPosition[1]);
+        int step = (nextPosition[1] - (*rook)->instantPosition[1]);
 
         for(int i =  1 * abs(step) / step; i != step + abs(step) / step; i += abs(step) / step)
         {
-            if(chessBoard[rook->instantPosition[0]][rook->instantPosition[1] + i] != defaultSymbol)
+            if(chessBoard[(*rook)->instantPosition[0]][(*rook)->instantPosition[1] + i] != defaultSymbol)
                 return 0;
         }
     }
-    else if(nextPosition[1] == rook->instantPosition[1])
+    else if(nextPosition[1] == (*rook)->instantPosition[1])
     {
-        int step = (nextPosition[0] - rook->instantPosition[0]);
+        int step = (nextPosition[0] - (*rook)->instantPosition[0]);
 
         for(int i =  1 * abs(step) / step; i != step + abs(step) / step; i += abs(step) / step)
         {
-            if(chessBoard[rook->instantPosition[0] + i][rook->instantPosition[1]] != defaultSymbol)
+            if(chessBoard[(*rook)->instantPosition[0] + i][(*rook)->instantPosition[1]] != defaultSymbol)
                 return 0;
         }
     }
@@ -92,23 +119,23 @@ int RookMotion(struct chessPiece *rook, char chessBoard[8][8], int nextPos, char
         return 0;
     }
     
-    if(rook->firstMove == 1)
+    if((*rook)->firstMove == 1)
     {
-        rook->firstMove = 0;
+        (*rook)->firstMove = 0;
     }
     
     return 1;
 }
 
-int HorseMotion(struct chessPiece *horse, char chessBoard[8][8], int nextPos, char defaultSymbol)
+int HorseMotion(struct chessPiece **horse, char chessBoard[8][8], int nextPos, char defaultSymbol)
 {
-    double distance = sqrt(pow(horse->instantPosition[0] - (nextPos / 10), 2) + pow(horse->instantPosition[1] - (nextPos % 10), 2));
+    double distance = sqrt(pow((*horse)->instantPosition[0] - (nextPos / 10), 2) + pow((*horse)->instantPosition[1] - (nextPos % 10), 2));
 
     if(distance == sqrt(5) && (chessBoard[nextPos / 10][nextPos % 10] == defaultSymbol))
     {
-        if(horse->firstMove == 1)
+        if((*horse)->firstMove == 1)
         {
-            horse->firstMove = 0;
+            (*horse)->firstMove = 0;
         }
 
         return 1;
@@ -117,19 +144,19 @@ int HorseMotion(struct chessPiece *horse, char chessBoard[8][8], int nextPos, ch
     return 0;
 }
 
-int BishopMotion(struct chessPiece *bishop, char chessBoard[8][8], int nextPos, char defaultSymbol)
+int BishopMotion(struct chessPiece **bishop, char chessBoard[8][8], int nextPos, char defaultSymbol)
 {
     //Think in reverse for the y-axis
     int iX = 1, iY = 1;
-    int tempBishopPositionY = bishop->instantPosition[0];
-    int tempBishopPositionX = bishop->instantPosition[1];
+    int tempBishopPositionY = (*bishop)->instantPosition[0];
+    int tempBishopPositionX = (*bishop)->instantPosition[1];
 
-    if(bishop->instantPosition[0] > nextPos / 10)
+    if((*bishop)->instantPosition[0] > nextPos / 10)
     {
         iY = -1;
     }
 
-    if(bishop->instantPosition[1] > nextPos % 10)
+    if((*bishop)->instantPosition[1] > nextPos % 10)
     {
         iX = -1;
     }
@@ -141,9 +168,9 @@ int BishopMotion(struct chessPiece *bishop, char chessBoard[8][8], int nextPos, 
 
         if((tempBishopPositionY == nextPos / 10) && (tempBishopPositionX == nextPos % 10))
         {
-            if(bishop->firstMove == 1)
+            if((*bishop)->firstMove == 1)
             {
-                bishop->firstMove = 0;
+                (*bishop)->firstMove = 0;
             }
 
             return 1;
@@ -153,15 +180,15 @@ int BishopMotion(struct chessPiece *bishop, char chessBoard[8][8], int nextPos, 
     return 0;
 }
 
-int KingMotion(struct chessPiece *king, char chessBoard[8][8], int nextPos, char defaultSymbol)
+int KingMotion(struct chessPiece **king, char chessBoard[8][8], int nextPos, char defaultSymbol)
 {
-    double distance = sqrt(pow(king->instantPosition[0] - (nextPos / 10), 2) + pow(king->instantPosition[1] - (nextPos % 10), 2));
+    double distance = sqrt(pow((*king)->instantPosition[0] - (nextPos / 10), 2) + pow((*king)->instantPosition[1] - (nextPos % 10), 2));
 
     if(distance <= sqrt(2) && (chessBoard[nextPos / 10][nextPos % 10] == defaultSymbol))
     {
-        if(king->firstMove == 1)
+        if((*king)->firstMove == 1)
         {
-            king->firstMove = 0;
+            (*king)->firstMove = 0;
         }
 
         return 1;
--- MainChess.c
+++ MainChess.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include "BoardLayoutAndPieces.h"
 
+void PieceSwitchFunction(struct chessPiece **, int *, int, int, char ,char [8][8]);
+void FindSelectedPiece(struct chessPiece[], struct chessPiece[], int, struct chessPiece **);
 void CreateChessBoard(char[8][8], int, char);
 void PrintChessBoard(char[8][8], int);
 void ClearTerminal();
@@ -11,162 +13,172 @@ int main()
     char chessBoard[8][8];
     int lengthBoard = sizeof(chessBoard) / sizeof(chessBoard[0]); //Length -> x = y
     char defaultSymbol = 'O';
-    int numberPieces = 16;
+    int isWhiteTurn = 1;
 
     //Prepare the board
     CreateChessBoard(chessBoard, lengthBoard, defaultSymbol);
 
-    struct chessPiece pawn[8];
+    //WHITE PIECES
+    struct chessPiece whitePawns[8];
 
-    /*
-    struct chessPiece rightRook = {{7, 0}, {7, 0}, 'R'}; LocatePieceOnBoard(&rightRook, chessBoard);
-    struct chessPiece leftRook = {{7, 7}, {7, 7}, 'R'}; LocatePieceOnBoard(&leftRook, chessBoard);
-    struct chessPiece rightHorse = {{7, 1}, {7, 1}, 'H'}; LocatePieceOnBoard(&rightHorse, chessBoard);
-    struct chessPiece leftHorse = (struct chessPiece){{7, 6}, {7, 6}, 'H'}; LocatePieceOnBoard(&leftHorse, chessBoard);
-    struct chessPiece rightBishop = (struct chessPiece){{7, 2}, {7, 2}, 'B'}; LocatePieceOnBoard(&rightBishop, chessBoard);
-    struct chessPiece leftBishop = (struct chessPiece){{7, 5}, {7, 5}, 'B'}; LocatePieceOnBoard(&leftBishop, chessBoard);
-    struct chessPiece queen = (struct chessPiece){{7, 3}, {7, 3}, 'Q'}; LocatePieceOnBoard(&queen, chessBoard);
-    struct chessPiece king = (struct chessPiece){{7, 4}, {7, 4}, 'K'}; LocatePieceOnBoard(&king, chessBoard);
-    */
-
-    struct chessPiece majorPieces[] = 
+    struct chessPiece whiteMajorPieces[] = 
     {
-        {{7, 0}, {7, 0}, 'R', 1}, //rightRook
-        {{7, 7}, {7, 7}, 'R', 1}, //leftRook
-        {{7, 1}, {7, 1}, 'H', 1}, //rightHorse
-        {{7, 6}, {7, 6}, 'H', 1}, //leftHorse
-        {{7, 2}, {7, 2}, 'B', 1}, //rightBishop
-        {{7, 5}, {7, 5}, 'B', 1}, //leftBishop
-        {{7, 3}, {7, 3}, 'Q', 1}, //queen
-        {{7, 4}, {7, 4}, 'K', 1}  //king
+        {{7, 0}, {7, 0}, 'R', 1, 1}, //rightRook
+        {{7, 7}, {7, 7}, 'R', 1, 1}, //leftRook
+        {{7, 1}, {7, 1}, 'H', 1, 1}, //rightHorse
+        {{7, 6}, {7, 6}, 'H', 1, 1}, //leftHorse
+        {{7, 2}, {7, 2}, 'B', 1, 1}, //rightBishop
+        {{7, 5}, {7, 5}, 'B', 1, 1}, //leftBishop
+        {{7, 3}, {7, 3}, 'Q', 1, 1}, //queen
+        {{7, 4}, {7, 4}, 'K', 1, 1}  //king
+    };
+
+    //BLACK PIECES
+    struct chessPiece blackPawns[8];
+
+    struct chessPiece blackMajorPieces[] = 
+    {
+        {{0, 0}, {0, 0}, 'R', 1, 0}, //rightRook
+        {{0, 7}, {0, 7}, 'R', 1, 0}, //leftRook
+        {{0, 1}, {0, 1}, 'H', 1, 0}, //rightHorse
+        {{0, 6}, {0, 6}, 'H', 1, 0}, //leftHorse
+        {{0, 2}, {0, 2}, 'B', 1, 0}, //rightBishop
+        {{0, 5}, {0, 5}, 'B', 1, 0}, //leftBishop
+        {{0, 3}, {0, 3}, 'Q', 1, 0}, //queen
+        {{0, 4}, {0, 4}, 'K', 1, 0}  //king
     };
 
     //Locate all major pieces on chessBoard
     for(int i = 0; i < lengthBoard; i++)
     {
-        LocatePieceOnBoard(&majorPieces[i], chessBoard);
+        LocatePieceOnBoard(&whiteMajorPieces[i], chessBoard);
+        LocatePieceOnBoard(&blackMajorPieces[i], chessBoard);
     }
 
     //Create Pawns
     for(int i = 0; i < lengthBoard; i++)
     {
-        CreatePawns(&pawn[i], 60 + i);
-        LocatePieceOnBoard(&pawn[i], chessBoard);
-        pawn[i].firstMove = 1;
+        whitePawns[i].isWhite = 1;
+        CreatePawns(&whitePawns[i], 60 + i);
+        LocatePieceOnBoard(&whitePawns[i], chessBoard);
+        whitePawns[i].firstMove = 1;
+
+        blackPawns[i].isWhite = 0;
+        CreatePawns(&blackPawns[i], 10 + i);
+        LocatePieceOnBoard(&blackPawns[i], chessBoard);
+        blackPawns[i].firstMove = 1;
     }
  
     PrintChessBoard(chessBoard, lengthBoard); 
 
     int selectedPosition;
     int nextPosition;
+
+    struct chessPiece *selectedPiece = (struct chessPiece *)malloc(sizeof(struct chessPiece));
+
     while(1)
     {
+        if(isWhiteTurn)
+            printf("Turn of WHITE\n");
+        else
+            printf("Turn of BLACK\n");
+
         //Take input from user to play
         printf("Select your piece: ");
         scanf("%d", &selectedPosition);
         printf("Input its next position: ");
         scanf("%d", &nextPosition);
 
-        char selectedPiece = chessBoard[selectedPosition / 10][selectedPosition % 10];
-        printf("%c\n", selectedPiece);
-
-        switch (selectedPiece)
+        //Check the turn and find selected piece
+        if(isWhiteTurn)
         {
-            case 'P':
-                for(int i = 0; i < 8; i++)
-                {
-                    if((pawn[i].instantPosition[0] == selectedPosition / 10) && (pawn[i].instantPosition[1] == selectedPosition % 10) && PawnMotion(&pawn[i], nextPosition))
-                    {
-                        chessBoard[pawn[i].instantPosition[0]][pawn[i].instantPosition[1]] = defaultSymbol; //Clear old position
-                        ChangePositionOfPiece(&pawn[i], nextPosition);
-                        chessBoard[pawn[i].instantPosition[0]][pawn[i].instantPosition[1]] = pawn[i].symbol;
-                    } 
-                }
-                break;
-
-            case 'R':         
-                //If it is right rook:
-                if((majorPieces[0].instantPosition[0] == selectedPosition / 10) && (majorPieces[0].instantPosition[1] == selectedPosition % 10))
-                {
-                    if(RookMotion(&majorPieces[0], chessBoard, nextPosition, defaultSymbol))
-                    {
-                        MovePieceAndSetBoard(&majorPieces[0], chessBoard, nextPosition, defaultSymbol);
-                    }
-                }
-                //If it is left rook:
-                else if((majorPieces[1].instantPosition[0] == selectedPosition / 10) && (majorPieces[1].instantPosition[1] == selectedPosition % 10))
-                {
-                    if(RookMotion(&majorPieces[1], chessBoard, nextPosition, defaultSymbol))
-                    {
-                        MovePieceAndSetBoard(&majorPieces[1], chessBoard, nextPosition, defaultSymbol);
-                    }
-                }
-                break;
-
-            case 'H':
-                //If it is right horse
-                if((majorPieces[2].instantPosition[0] == selectedPosition / 10) && (majorPieces[2].instantPosition[1] == selectedPosition % 10))
-                {
-                    if(HorseMotion(&majorPieces[2], chessBoard, nextPosition, defaultSymbol))
-                    {
-                        MovePieceAndSetBoard(&majorPieces[2], chessBoard, nextPosition, defaultSymbol);
-                    }
-                }
-                //If it is left horse
-                else if((majorPieces[3].instantPosition[0] == selectedPosition / 10) && (majorPieces[3].instantPosition[1] == selectedPosition % 10))
-                {
-                    if(HorseMotion(&majorPieces[3], chessBoard, nextPosition, defaultSymbol))
-                    {
-                        MovePieceAndSetBoard(&majorPieces[3], chessBoard, nextPosition, defaultSymbol);
-                    }
-                }
-                break;
-            
-            case 'B':
-                //If it is right bishop
-                if((majorPieces[4].instantPosition[0] == selectedPosition / 10) && (majorPieces[4].instantPosition[1] == selectedPosition % 10))
-                {
-                    if(BishopMotion(&majorPieces[4], chessBoard, nextPosition, defaultSymbol))
-                    {
-                        MovePieceAndSetBoard(&majorPieces[4], chessBoard, nextPosition, defaultSymbol);
-                    }
-                }
-                //If it is left bishop
-                else if((majorPieces[5].instantPosition[0] == selectedPosition / 10) && (majorPieces[5].instantPosition[1] == selectedPosition % 10))
-                {
-                    if(BishopMotion(&majorPieces[5], chessBoard, nextPosition, defaultSymbol))
-                    {
-                        MovePieceAndSetBoard(&majorPieces[5], chessBoard, nextPosition, defaultSymbol);
-                    }
-                }
-                break;
-
-            case 'Q':
-                //Calling horse and bishop functions
-                if(BishopMotion(&majorPieces[6], chessBoard, nextPosition, defaultSymbol) || RookMotion(&majorPieces[6], chessBoard, nextPosition, defaultSymbol))
-                {
-                    MovePieceAndSetBoard(&majorPieces[6], chessBoard, nextPosition, defaultSymbol);
-                }
-
-            case 'K':
-                if(KingMotion(&majorPieces[7], chessBoard, nextPosition, defaultSymbol))
-                {
-                    MovePieceAndSetBoard(&majorPieces[7], chessBoard, nextPosition, defaultSymbol);
-                }
-            
-            //OTHER CASES
-
-
-            default:
-                break;
+            FindSelectedPiece(whitePawns, whiteMajorPieces, selectedPosition, &selectedPiece);
         }
+        else
+        {
+            FindSelectedPiece(blackPawns, blackMajorPieces, selectedPosition, &selectedPiece);
+        }
+
+        PieceSwitchFunction(&selectedPiece, &isWhiteTurn, selectedPosition, nextPosition, defaultSymbol, chessBoard);
+
+        isWhiteTurn = !isWhiteTurn;
 
         PrintChessBoard(chessBoard, lengthBoard); 
     }
     
-        
-
     return 0;
+}
+
+void PieceSwitchFunction(struct chessPiece **selectedPiece, int *isWhiteTurn, int selectedPosition, int nextPosition, char defaultSymbol, char chessBoard[8][8])
+{
+    switch ((*selectedPiece)->symbol)
+    {
+        case 'P':
+            if (PawnMotion(selectedPiece, nextPosition))
+            {
+                MovePieceAndSetBoard(selectedPiece, chessBoard, nextPosition, defaultSymbol);
+            }
+
+            break;
+        
+        case 'R':         
+            if(RookMotion(selectedPiece, chessBoard, nextPosition, defaultSymbol))
+            {
+                MovePieceAndSetBoard(selectedPiece, chessBoard, nextPosition, defaultSymbol);
+            }
+
+            break;
+
+        case 'H':
+            if(HorseMotion(selectedPiece, chessBoard, nextPosition, defaultSymbol))
+            {
+                MovePieceAndSetBoard(selectedPiece, chessBoard, nextPosition, defaultSymbol);
+            }
+
+            break;
+        
+        case 'B':
+            if(BishopMotion(selectedPiece, chessBoard, nextPosition, defaultSymbol))
+            {
+                MovePieceAndSetBoard(selectedPiece, chessBoard, nextPosition, defaultSymbol);
+            }
+    
+            break;
+
+        case 'Q':
+            //Calling horse and bishop functions
+            if(BishopMotion(selectedPiece, chessBoard, nextPosition, defaultSymbol) || RookMotion(selectedPiece, chessBoard, nextPosition, defaultSymbol))
+            {
+                MovePieceAndSetBoard(selectedPiece, chessBoard, nextPosition, defaultSymbol);
+            }
+
+        case 'K':
+            if(KingMotion(selectedPiece, chessBoard, nextPosition, defaultSymbol))
+            {
+                MovePieceAndSetBoard(selectedPiece, chessBoard, nextPosition, defaultSymbol);
+            }
+        
+        default:
+            break;
+    }
+}
+
+void FindSelectedPiece(struct chessPiece pawns[], struct chessPiece majorPieces[], int selectedPos, struct chessPiece **selectedPiece)
+{
+    for(int i = 0; i < 8; i++)
+    {
+        if(((pawns[i].instantPosition[0]) == selectedPos / 10) && ((pawns[i].instantPosition[1]) == (selectedPos % 10)))
+        {
+            *selectedPiece = &pawns[i];
+            return;
+        }
+
+        if((majorPieces[i].instantPosition[0] == selectedPos / 10) && (majorPieces[i].instantPosition[1] == selectedPos % 10))
+        {
+            *selectedPiece = &majorPieces[i];
+            return;
+        }
+    }
 }
 
 void CreateChessBoard(char board[8][8], int length, char defaultSymbol)
